b89bc3edc26874b8a1213e2e4e2f33255f14ae52
Bharatkumar17 <70887566+Bharatkumar17@users.noreply.github.com>
2025-05-31 18:31:26 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,40 +1,33 @@
+from datetime import datetime  # यह लाइन जरूर जोड़ें
 from pymongo import MongoClient
-from typing import Optional, List, Dict
+from config import Config
 
-class MongoDB:
-    def __init__(self, uri: str):
-        self.client = MongoClient(uri)
-        self.db = self.client["drm_uploader"]
-        self.users = self.db["users"]
-        self.activities = self.db["activities"]
+class Database:
+    def __init__(self):
+        self.client = MongoClient(Config.MONGO_URI)
+        self.db = self.client.course_extractor_bot
     
-    def is_user_authorized(self, user_id: int) -> bool:
-        """Check if user is authorized to use the bot"""
-        user = self.users.find_one({"user_id": user_id})
-        return user is not None
+    def add_user(self, user_id, username, first_name, last_name):
+        users = self.db.users
+        user_data = {
+            'user_id': user_id,
+            'username': username,
+            'first_name': first_name,
+            'last_name': last_name,
+            'joined_at': datetime.now()
+        }
+        users.update_one({'user_id': user_id}, {'$set': user_data}, upsert=True)
     
-    def log_activity(self, user_id: int, platform: str, content_title: str):
-        """Log user activity"""
-        self.activities.insert_one({
-            "user_id": user_id,
-            "platform": platform,
-            "content_title": content_title,
-            "timestamp": datetime.utcnow()
-        })
+    def log_request(self, user_id, platform, course_name):
+        requests = self.db.requests
+        request_data = {
+            'user_id': user_id,
+            'platform': platform,
+            'course_name': course_name,
+            'requested_at': datetime.now()
+        }
+        requests.insert_one(request_data)
     
-    def get_user_activities(self, user_id: int) -> List[Dict]:
-        """Get all activities for a user"""
-        return list(self.activities.find({"user_id": user_id}).sort("timestamp", -1))
-    
-    def add_user(self, user_id: int, username: Optional[str] = None):
-        """Add a new authorized user"""
-        if not self.is_user_authorized(user_id):
-            self.users.insert_one({
-                "user_id": user_id,
-                "username": username,
-                "added_at": datetime.utcnow()
-            })
-    
-    def remove_user(self, user_id: int):
-        """Remove user authorization"""
-        self.users.delete_one({"user_id": user_id})
+    def get_user_stats(self, user_id):
+        requests = self.db.requests
+        return requests.count_documents({'user_id': user_id})
